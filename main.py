06d6398d3ef6be67476fa13f0218cc14e5178439
Murillo Maiorano <79369489+maioranomu@users.noreply.github.com>
2025-02-19 15:09:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os,time,random
 def clear(): time.sleep(0.1); os.system("cls")
 truck1,truck2,truck3,b1,b2,b3,counter,s1,s2,s3,s4,sky1,sky2,sky3,sky4,c11,c12,c13,c14,c21,c22,c23,c24 = ("   ________________","  | ##### TRUCK  |o\\","  |_________________|","_"*2,"_"*11,"_"*100," ","(|)","(/)","(-)","(\\)"," "*20," "*20," "*20," "*20,"    ___  ___  "," __/   \\/   \\ ","/            \\","\\____________/","  ___  _____  "," /   \\/     \\ ","/            \\","\\____________/");sprites = [f"{b1}{s}{b2}{s}{b3}" for s in (s1, s2, s3, s4)]
-while 1:
+while True:
     for s in sprites:
         clear(); counter += "_"
         if len(counter) % 10 == 0:
